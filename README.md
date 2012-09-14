8f2b20d9365e458121911feb86b463653ef6268e
Janus <ysangkok@gmail.com>
2012-09-14 03:17:22 +0200

    seperate queries

--- README.md
+++ README.md
@@ -5,15 +5,16 @@ Instructions
 1. ``cd datalog.js``
 1. ``wget -O - "http://downloads.sourceforge.net/project/datalog/datalog/2.2/datalog-2.2.tar.gz" | tar xz``
 1. ``cd datalog-2.2``
-1. ``patch try.sh < ~/Downloads/datalog.js/jsify_tests.patch``
-1. ``patch interp.c < ~/Downloads/datalog.js/force_cmdline_param.patch``
-1. ``patch lua/lua.c < ~/Downloads/datalog.js/rename_lua_c_main.patch``
-1. ``patch lua/luac.c < ~/Downloads/datalog.js/rename_lua_c_main.patch``
+1. ``patch try.sh < ../jsify_tests.patch``
+1. ``patch interp.c < ../force_cmdline_param.patch``
+1. ``patch lua/lua.c < ../rename_lua_main.patch``
+1. ``patch lua/luac.c < ../rename_luac_main.patch``
 1. ``emconfigure ./configure && make``
 1. ``chmod +x bin2c``
 1. ``make``
 1. ``ln -s datalog datalog.bc``
 1. ``emcc -O2 --pre-js ../prejs.js -o datalog.js datalog.bc``
-1. ``./try.sh``
+1. ``chmod +x try.sh``
+1. ``./try.sh || echo "Tests failed!"``
 1. ``cd ..``
 1. ``./wrap.sh``
--- wrapper.js
+++ wrapper.js
@@ -1,3 +1,15 @@
+function tab(id) {
+  if (id === "#t1")
+    $("#tabcontainer").addClass("taboneactive");
+  else
+    $("#tabcontainer").removeClass("taboneactive");
+}
+var jump = function(line) {
+      editor.setCursor(Number(line),0);
+      editor.setSelection({line:Number(line),ch:0},{line:Number(line)+1,ch:0});
+      editor.focus();
+}
+
 var get = function(url) {
     var outerRes;
     jQuery.ajax({
@@ -12,9 +24,12 @@ var get = function(url) {
 };
 
 function addText(elId, text) {
+    $("#" + elId).append(text.replace("\n","<br>"))
+/*
     var obj = document.getElementById(elId);
     var txt = document.createTextNode(text);
     obj.appendChild(txt);
+*/
 }
 
 function trimLines(str) {
@@ -25,32 +40,33 @@ function trimLines(str) {
 var htmlEscape = function(v) {
     return v.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 };
-var fileContent;
+var datalogContent, luaContent, queryContent;
 var Module = {};
 //Module["noExitRuntime"] = true;
 Module["noInitialRun"] = true;
 Module["print"] = function(v) {
     addText("output", v + "\n");
 };
-Module["arguments"] = ['-l', '/lib.lua', '/datalog.dl'];
+Module["arguments"] = ['-i', '-l', '/lib.lua', '/datalog.dl', '/queries.dl'];
 window["wrapperPreInit"] = function(FS) {
     //window.FS = FS;
-    FS.createDataFile('/', 'datalog.dl', fileContent, true, false);
-    FS.createDataFile('/', 'lib.lua', get("default.lua"), true, false);
+    FS.createDataFile('/', 'datalog.dl', datalogContent, true, false);
+    FS.createDataFile('/', 'queries.dl', queryContent, true, false);
+    FS.createDataFile('/', 'lib.lua', luaContent, true, false);
 }
 var data = {
     'ancestor': trimLines("% Equality test\n\
-		ancestor(A, B) :-\n\
-		    parent(A, B).\n\
-		ancestor(A, B) :-\n\
-		    parent(A, C),\n\
-		    D = C,      % Unification required\n\
-		    ancestor(D, B).\n\
-		parent(john, douglas).\n\
-		parent(bob, john).\n\
-		parent(ebbon, bob).\n\
-		ancestor(A, B)?\n\
-	"),
+      ancestor(A, B) :-\n\
+        parent(A, B).\n\
+      ancestor(A, B) :-\n\
+        parent(A, C),\n\
+        D = C,      % Unification required\n\
+        ancestor(D, B).\n\
+      parent(john, douglas).\n\
+      parent(bob, john).\n\
+      parent(ebbon, bob).\n\
+      ancestor(A, B)?\n\
+    "),
     'bidipath': "% path test from Chen & Warren\nedge(a, b). edge(b, c). edge(c, d). edge(d, a).\npath(X, Y) :- edge(X, Y).\npath(X, Y) :- edge(X, Z), path(Z, Y).\npath(X, Y) :- path(X, Z), edge(Z, Y).\npath(X, Y)?\n",
     'laps': "% Laps Test\ncontains(ca, store, rams_couch, rams).\ncontains(rams, fetch, rams_couch, will).\ncontains(ca, fetch, Name, Watcher) :-\n    contains(ca, store, Name, Owner),\n    contains(Owner, fetch, Name, Watcher).\ntrusted(ca).\npermit(User, Priv, Name) :-\n    contains(Auth, Priv, Name, User),\n    trusted(Auth).\npermit(User, Priv, Name)?\n",
     'long': "abcdefghi(z123456789,\nz1234567890123456789,\nz123456789012345678901234567890123456789,\nz1234567890123456789012345678901234567890123456789012345678901234567890123456789).\n\nthis_is_a_long_identifier_and_tests_the_scanners_concat_when_read_with_a_small_buffer.\nthis_is_a_long_identifier_and_tests_the_scanners_concat_when_read_with_a_small_buffer?\n",
@@ -67,28 +83,73 @@ var data = {
 data["u7"] = get("mitre-u7.dl");
 
 
-var editor;
+var editor, luaEditor, queryEditor;
 var onLoad = function() {
+    jQuery("#codeform").submit(function(e) { execute(); });
+
+    jQuery(window).resize(function(e) {
+      var val = "calc(" + $("#datalogpane").height() + "px - 2.6em)";
+      if (jQuery.browser.webkit) val = "-webkit-" + val;
+      $(editor.getScrollerElement()).css("height", val);
+      $(luaEditor.getScrollerElement()).css("height", val);
+      $(queryEditor.getScrollerElement()).css("height", val);
+    });
+
     editor = CodeMirror.fromTextArea(document.getElementById("datalog"), {
         mode: "clike",
         lineNumbers: true
     });
-
+    queryEditor = CodeMirror.fromTextArea(document.getElementById("queries"), {
+        mode: "clike",
+        lineNumbers: true
+    });
+    luaEditor = CodeMirror.fromTextArea(document.getElementById("lua"), {
+        mode: "lua",
+        lineNumbers: true
+    });
+    luaEditor.setValue(get("default.lua"));
+    $(window).resize();
+    document.getElementById("submitbutton").disabled = false;
     switc("ancestor");
-
 };
-var submit = function() {
+var execute = function() {
     if (document.getElementById("submitbutton").disabled) throw new Error("disabled!");
     document.getElementById("submitbutton").disabled = true;
-    fileContent = editor.getValue();
+    try {
+      datalogContent = editor.getValue();
+    } catch (err) {
+      datalogContent = document.getElementById("datalog").value;
+    }
+    try {
+      luaContent = luaEditor.getValue();
+    } catch (err) {
+      luaContent = document.getElementById("lua").value;
+    }
+    try {
+      queryContent = queryEditor.getValue();
+    } catch (err) {
+      queryContent = document.getElementById("queries").value;
+    }
     var Module = CompiledModule();
     document.getElementById("output").innerHTML = "";
     var rc = Module["run"]();
     document.getElementById("output").style.color = (rc === 0) ? "inherit" : "red";
+    if (rc !== 0) {
+      // find error line and highlight
+      var re = /\/datalog.dl:(\d+)/;
+      var arr = document.getElementById("output").innerHTML.match(re); // ["/datalog.dl:12", "12"] 
+      jump(Number(arr[1])-1);
+    }
     document.getElementById("submitbutton").disabled = false;
+    return false;
 };
 
 function switc(v) {
-    editor.setValue(data[v]);
-    submit();
+    try {
+      editor.setValue(data[v]);
+    } catch (err) {
+      document.getElementById("datalog").value = data[v];
+    }
+    execute();
 }
+
